f24c745340f10455b3b507569fda224f57c5581e
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2024-05-28 02:24:40 +0000

    fixes

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArr = [];
-    console.log("array.keys()")
-    console.log(array.keys())
-    for (let value in array) {
-      newArr.push(appendString + value)
-    }
-  
-    return newArr;
+  const newArr = [];
+  console.log("array.keys()");
+  console.log(array.keys());
+  for (let value in array) {
+    newArr.push(appendString + value);
   }
+
+  return newArr;
+}
