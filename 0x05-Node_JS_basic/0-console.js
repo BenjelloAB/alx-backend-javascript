977225944436abfb27d0e59855b8cc755dd3040c
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2024-11-26 19:49:59 +0100

    node js basics comeback

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the stdoutput
+ * @param {String} msg The message to display
+ *
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
